cc260f1f9c05e9963b89ec99c70b50d57c2f5bb5
CoderSPM <mohantyswayam2001@gmail.com>
2025-07-24 16:12:30 +0530

    Added Validation

--- src/main/java/com/cozentus/pms/controllers/SkillsController.java
+++ src/main/java/com/cozentus/pms/controllers/SkillsController.java
@@ -1,5 +1,7 @@
 package com.cozentus.pms.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,18 +19,36 @@ public class SkillsController {
 		this.skillService = skillService;
 	}
 	@PostMapping
-	public void createNewSkill(@RequestParam String skillName) {
-		skillService.createNewSkill(skillName);
+	public ResponseEntity<?> createNewSkill(@RequestParam String skillName) {
+	    if (skillName == null || skillName.trim().isEmpty()) {
+	        return ResponseEntity.badRequest().body("Skill name must not be blank.");
+	    }
+		skillService.createNewSkill(skillName.toUpperCase());
+		return ResponseEntity.ok("Skill updated successfully.");
 	}
 	
 	@PutMapping
-	public void updateSkill(@RequestParam String oldSkillName, @RequestParam String newSkillName) {
-		skillService.updateSkill(oldSkillName, newSkillName);
+	public ResponseEntity<?> updateSkill(@RequestParam String oldSkillName, @RequestParam String newSkillName) {
+	    if (oldSkillName == null || oldSkillName.trim().isEmpty() ) {
+	        return ResponseEntity.badRequest().body("Old Skill name must not be blank.");
+	    }
+	    else if (newSkillName == null || newSkillName.trim().isEmpty()) {
+	        return ResponseEntity.badRequest().body("New skill name must not be blank.");
+	    }
+	    else if (oldSkillName.equalsIgnoreCase(newSkillName)) {
+	        return ResponseEntity.badRequest().body("New skill name must be different from the old skill name.");
+	    }
+		skillService.updateSkill(oldSkillName, newSkillName.toUpperCase());
+		return ResponseEntity.ok("Skill updated successfully.");
 	}
 	
 	@DeleteMapping
-	public void deleteSkill(@RequestParam String skillName) {
+	public ResponseEntity<?> deleteSkill(@RequestParam String skillName) {
+			    if (skillName == null || skillName.trim().isEmpty()) {
+	        return ResponseEntity.badRequest().body("Skill name must not be blank.");
+	    }
 		skillService.deleteSkill(skillName);
+		return ResponseEntity.ok("Skill deleted successfully.");
 	}
 	
 }
